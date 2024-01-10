3793e0796e5f5859f301dec745308c25ecfbb63b
Victor Gomez <elbebi.py@gmail.com>
2024-01-10 09:31:53 -0300

    backend pushed to personal remote repo

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 
+const cors = require('cors')
+
+app.use(cors())
+
+app.use(express.static('dist'))
+
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
   console.log('Path:  ', request.path)
@@ -93,7 +99,7 @@ app.delete('/api/notes/:id', (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
